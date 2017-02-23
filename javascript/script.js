25d72573822acbe642c91fc85492c7b058becad4
Mark5795 <markkea95@gmail.com>
2017-02-23 19:08:16 +0100

    Dingen laten bewegen

--- javascript/script.js
+++ javascript/script.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
 	$('.php').animate({width: '75%'},6000);
 	$('.js').animate({width: '75%'},6000);
 	$('.photoshop').animate({width: '65%'},6000);
+
+	$('#arrow, .skills, #submit').mouseenter(function() {
+       $(this).animate({
+           height: '+=10px'
+       });
+   });
+   $('#arrow, .skills, #submit').mouseleave(function() {
+       $(this).animate({
+           height: '-=10px'
+       }); 
+   });
 });
